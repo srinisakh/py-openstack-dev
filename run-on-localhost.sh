bc5f68e19d32eb287cbd56d076e87f28cb4ef14a
srsakhamuri <sr.sakhamuri@gmail.com>
2015-06-28 13:24:23 -0600

    Update run-on-localhost.sh

--- run-on-localhost.sh
+++ run-on-localhost.sh
@@ -1,5 +1,6 @@
 # Run this shell script to setup the python environment on your localhost
 # Troubleshooting tip: Make sure you have at least 1MB memory allocated, otherwise ceilo dependencies fail to install
+export ANSIBLE_HOST_KEY_CHECKING=False
 sudo apt-get -y install sshpass ansible
 ansible-galaxy install srsakhamuri.py-openstack-dev -p ~
 cd ~/srsakhamuri.py-openstack-dev/
